3639b8cc4c59cbf5656221f8c0e0c51cf49d99db
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-10-29 15:25:04 -0700

    limit amt. of media on front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @movies = Movie.all.sort_by { |movie| movie.rank }.reverse
-    @books = Book.all.sort_by { |book| book.rank }.reverse
-    @albums = Album.all.sort_by { |album| album.rank }.reverse
+    @movies = Movie.all.limit(10).sort_by { |movie| movie.rank }.reverse
+    @books = Book.all.limit(10).sort_by { |book| book.rank }.reverse
+    @albums = Album.all.limit(10).sort_by { |album| album.rank }.reverse
   end
 end
