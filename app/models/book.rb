e8a46e6b7abf806162fe11977176aff2cd736519
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-10-29 14:56:32 -0700

    add model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
